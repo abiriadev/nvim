b0aac5ec26a152ef56c06dc268a5d00fc91bd131
Abiria <abiria.dev@gmail.com>
2024-03-10 11:22:47 +0900

    feat: rearrange telescope layout

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,14 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  opts = {
+    defaults = {
+      layout_strategy = "vertical",
+      layout_config = {
+        vertical = {
+          mirror = true,
+          width = 0.5,
+        },
+      },
+    },
+  },
+}
