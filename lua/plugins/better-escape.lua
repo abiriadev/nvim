7590c74047777a0c5a20b5b272e7b8cdce6d698b
Abiria <abiria.dev@gmail.com>
2024-03-12 10:01:22 +0900

    feat: use better-escape

---
+++ lua/plugins/better-escape.lua
@@ -0,0 +1,10 @@
+return {
+	"max397574/better-escape.nvim",
+	opts = {
+		mapping = {
+			"jk",
+			"jj",
+		}, -- a table with mappings to use
+		timeout = 1000, -- vim.o.timeoutlen, -- the time in which the keys must be hit in ms. Use option timeoutlen by default
+	},
+}
