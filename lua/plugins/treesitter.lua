4304a610cef0a034266ea24f0e06f1ca988f1eed
Abiria <abiria.dev@gmail.com>
2024-03-15 10:14:42 +0900

    feat: add treesitter parser for gotmpl and lalrpop

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,27 @@
+return {
+	{
+		"nvim-treesitter/nvim-treesitter",
+		config = function(_, opts)
+			require("nvim-treesitter.configs").setup(opts)
+
+			local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+
+			parser_config.gotmpl = {
+				install_info = {
+					url = "https://github.com/msvechla/tree-sitter-go-template",
+					files = { "src/parser.c" },
+				},
+				filetype = "gotmpl",
+				used_by = { "gohtmltmpl", "gotexttmpl", "gotmpl", "yaml" },
+			}
+
+			parser_config.gotmpl = {
+				install_info = {
+					url = "https://github.com/traxys/tree-sitter-lalrpop",
+					files = { "src/parser.c" },
+				},
+				filetype = "lalrpop",
+			}
+		end,
+	},
+}
