301cd688f61eef9266e8ac5109de43475fe00b95
Abiria <abiria.dev@gmail.com>
2024-04-21 08:23:24 +0900

    feat: support typespec formatter

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -6,6 +6,14 @@ return {
 			go = { "golines" },
 			fish = { "fish_indent" },
 			sh = { "shfmt" },
+			typespec = { "typespec" },
+		},
+		formatters = {
+			typespec = {
+				command = "tsp",
+				args = { "format", "$FILENAME" },
+				stdin = false,
+			},
 		},
 	},
 }
